e7a45ecd640c5173490e1828b2c8ab9e906ba2f9
Solomon Feldman <solomonrfeldman@gmail.com>
2019-10-17 02:34:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 main.remove()
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
-newHeader.innerHTML = ''
+newHeader.innerHTML = 'Hello yes, that is I, that is who is the champion.'
